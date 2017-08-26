564eb995b5de7f16fa4667162764edd912ddf824
Sachin <ohri.sachin@gmail.com>
2017-08-27 01:12:21 +0530

    route modified

--- Chapter 04/src/app/app.route.ts
+++ Chapter 04/src/app/app.route.ts
@@ -3,31 +3,27 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { NewsComponent } from './dashboard/news/news.component';
 
-const feedRoutes = [{
-  path: '',
-  component: NewsComponent
-}];
 
 const routes: Routes = [
   {path: '', redirectTo: 'nfl', pathMatch: 'full'},
   {
     path: 'nfl',
-    children: feedRoutes,
+    component: NewsComponent,
     data: {feedType: 'nfl-news',source:'nfl'}
   },
   {
     path: 'espn',
-    children: feedRoutes,
+    component: NewsComponent,
     data: {feedType: 'espn',source:'espn'}
   },
   {
     path: 'fox',
-    children: feedRoutes,
+    component: NewsComponent,
     data: {feedType: 'fox-sports',source:'Fox Sports'}
   },
   {
     path: 'bbc',
-    children: feedRoutes,
+    component: NewsComponent,
     data: {feedType: 'bbc-sport',source:'BBC Sports'}
   }
   
